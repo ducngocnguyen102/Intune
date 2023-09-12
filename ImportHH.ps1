d0447c241017b9578f55decd082b04d3d23b2090
ducngocnguyen102 <77482548+ducngocnguyen102@users.noreply.github.com>
2023-09-12 13:55:31 -0700

    Update ImportHH.ps1

--- ImportHH.ps1
+++ ImportHH.ps1
@@ -1,4 +1,5 @@
-﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force -Confirm:$false 
+[System.Net.ServicePointManager]::SecurityProtocol = 'TLS12'
+ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force -Confirm:$false 
 Install-PackageProvider -Name NuGet -force  
 Install-Script -Name Get-WindowsAutoPilotInfo -Force -Confirm:$False 
 Get-WindowsAutoPilotInfo -Online 
